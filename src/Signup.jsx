65f6de86d09aefe0bc6091817fa606d1eeddf105
zzpulkitzz <gpulkitgupta72@gmail.com>
2024-11-29 23:55:20 +0530

    minor change 2

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 import { useNavigate } from 'react-router-dom';
-import {HandleLoginContext} from "./login.jsx"
+import {HandleLoginContext} from "./Login.jsx"
 import { useContext } from 'react';
 
 
